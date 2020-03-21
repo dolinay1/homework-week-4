02bba17472e652117597c1fa80f3529b77b9b0f1
Julian Dolinay <dolinay1@gmail.com>
2020-03-21 13:07:04 -0400

    Updated html to starting page

---
+++ Develop/script.js
@@ -0,0 +1,4 @@
+// store quiz questions and answers in an object
+// make a for loop that iterates randomly through the object
+// when the user clicks submit
+// onclick for submit button to begin quiz
